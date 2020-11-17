69c346b885c4707b06ac018bfda7e7812506be14
Lexa070301 <Lexa070301@gmail.com>
2020-11-17 23:12:39 +0300

    app is ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,24 @@ import {MyDet} from './shared_models/det_model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  dets: MyDet[];
+  dets: MyDet[] = [];
+  cardname: string;
+
+  constructor() {
+    for (let i = 0; i < 10; i++) {
+      this.dets.push(new MyDet(i + 1, 'Det ' + (i + 1)));
+    }
+  }
+
+  deleteCard(id) {
+    for (let i = 0; i < this.dets.length; i++) {
+      if (this.dets[i].id == id) {
+        this.dets.splice(i, 1);
+      }
+    }
+  }
+
+  addCard() {
+    this.dets.push(new MyDet(this.dets[this.dets.length - 1].id + 1, this.cardname));
+  }
 }
--- src/app/shared_models/det_model.ts
+++ src/app/shared_models/det_model.ts
@@ -2,4 +2,17 @@ export class MyDet {
   id: number;
   name: string;
   status: boolean;
+  color: string;
+
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    if (Math.random() < 0.5) {
+      this.status = false;
+      this.color = 'red';
+    } else {
+      this.status = true;
+      this.color = 'green';
+    }
+  }
 }
